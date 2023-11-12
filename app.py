8edbbf0f22e01eea4191223ad7538f157f5224f9
amperstrand <amperstrand@localhost>
2023-11-12 21:23:37 +0100

    moved everything out of app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify
 import requests
-import os
-import json
 
 from price_api import get_price_from_bylls, get_price_from_bitbank, get_price_from_coingecko
 
@@ -25,46 +23,6 @@ def return_bitbank():
     return get_price_from_bitbank()
 
 if __name__ == '__main__':
-
-    coingecko_10x_output_file_path = '/tmp/exchange_rates_10x.json'
-    if not os.path.exists(coingecko_10x_output_file_path):
-        # Fetch exchange rates data
-        url = "https://api.coingecko.com/api/v3/exchange_rates"
-        response = requests.get(url)
-        exchange_rates = response.json()
-
-        # Process the data
-        for key, value in exchange_rates['rates'].items():
-            if value['type'] == "fiat":
-                value['value'] *= 10
-
-        # Save the processed data to a file
-        with open(coingecko_10x_output_file_path, 'w') as output_file:
-            json.dump(exchange_rates, output_file, indent=4)
-
-        print("Data from coingecko fetched and processed successfully.")
-
-
-    else:
-        print(f"{coingecko_10x_output_file_path} already exists. Skipping data fetch and processing.")
-
-    bitbank_output_file_path = '/tmp/bitbank.json'
-    if not os.path.exists(bitbank_output_file_path):
-        # Fetch exchange rates data
-        url = "https://public.bitbank.cc/tickers"
-        response = requests.get(url)
-        exchange_rates = response.json()
-
-        # Process the data
-        #for key, value in exchange_rates['rates'].items():
-        #    if value['type'] == "fiat":
-        #        value['value'] *= 10
-
-        # Save the processed data to a file
-        with open(bitbank_output_file_path, 'w') as bitbank_output_file_path:
-            json.dump(exchange_rates, bitbank_output_file_path, indent=4)
-
-        print("Data from bitbank fetched and processed successfully.")
 
 
     certfile = '/certificates/customrates.local.MITM.crt'
--- price_api.py
+++ price_api.py
@@ -1,12 +1,55 @@
-# price_api.py
-
 from flask import jsonify, request, send_from_directory
 import requests
+import os
+import json
 
 def get_price_from_coingecko():
+    coingecko_10x_output_file_path = '/tmp/exchange_rates_10x.json'
+    if not os.path.exists(coingecko_10x_output_file_path):
+        # Fetch exchange rates data
+        url = "https://api.coingecko.com/api/v3/exchange_rates"
+        response = requests.get(url)
+        exchange_rates = response.json()
+
+        # Process the data
+        for key, value in exchange_rates['rates'].items():
+            if value['type'] == "fiat":
+                value['value'] *= 10
+
+        # Save the processed data to a file
+        with open(coingecko_10x_output_file_path, 'w') as output_file:
+            json.dump(exchange_rates, output_file, indent=4)
+
+        print("Data from coingecko fetched and processed successfully.")
+
+
+    else:
+        print(f"{coingecko_10x_output_file_path} already exists. Skipping data fetch and processing.")
+
+
     return send_from_directory('/tmp', 'exchange_rates_10x.json')
 
 def get_price_from_bitbank():
+    bitbank_output_file_path = '/tmp/bitbank.json'
+    if not os.path.exists(bitbank_output_file_path):
+        # Fetch exchange rates data
+        url = "https://public.bitbank.cc/tickers"
+        response = requests.get(url)
+        exchange_rates = response.json()
+
+        # Process the data
+        #for key, value in exchange_rates['rates'].items():
+        #    if value['type'] == "fiat":
+        #        value['value'] *= 10
+
+        # Save the processed data to a file
+        with open(bitbank_output_file_path, 'w') as bitbank_output_file_path:
+            json.dump(exchange_rates, bitbank_output_file_path, indent=4)
+
+        print("Data from bitbank fetched and processed successfully.")
+
+
+
     return send_from_directory('/tmp', 'bitbank.json')
 
 def get_price_from_bylls():
